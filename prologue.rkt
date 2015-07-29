3eda475922712e0bd2e311e678477d80ca3a5456
krzychukula <krzychukula@gmail.com>
2015-07-29 15:06:52 +0200

    basics

--- prologue.rkt
+++ prologue.rkt
@@ -2,3 +2,23 @@
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-beginner-reader.ss" "lang")((modname prologue) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ())))
 (+ 1 1)
+(+ 2 2)
+(* 3 3)
+(- 4 2)
+(/ 6 2)
+(sqr 3);;9
+(expt 2 3);;8
+(sin 0);;0
+(cos pi);;#i-1.0
+
+(+ 2 (+ 3 4));;9
+(+ 2 3 4)
+
+(+ (* 5 5) (+ (* 3 (/ 12 4)) 4));; 38
+
+;;(+ (1) (2))
+;;function call: expected a function after the open parenthesis, but found a number
+
+
+(* 1 2 3 4 0);;0
+
