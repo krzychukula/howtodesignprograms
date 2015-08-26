5f7c810de2b4e97c22e2ba9ed00745787209864a
krzychukula <krzychukula@gmail.com>
2015-08-26 11:34:16 +0200

    exercise 52 - cover all conditions with tests

---
+++ 4/Exercise52.rkt
@@ -0,0 +1,17 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname Exercise52) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp")) #f)))
+; TrafficLight -> TrafficLight
+; determines the next state of the traffic light from the given s
+
+
+; cover all the cases with tests 
+(check-expect (traffic-light-next "red") "green")
+(check-expect (traffic-light-next "green") "yellow")
+(check-expect (traffic-light-next "yellow") "red")
+ 
+(define (traffic-light-next s)
+  (cond
+    [(string=? "red" s) "green"]
+    [(string=? "green" s) "yellow"]
+    [(string=? "yellow" s) "red"]))
