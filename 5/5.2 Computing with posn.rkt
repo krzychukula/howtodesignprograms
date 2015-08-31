a3e5389c23cc9d55dbb0b5ce87f3cd1ac399a1d0
krzychukula <krzychukula@gmail.com>
2015-08-31 10:56:10 +0200

    introduction of posn

---
+++ 5/5.2 Computing with posn.rkt
@@ -0,0 +1,7 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname |5.2 Computing with posn|) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp")) #f)))
+(define p (make-posn 31 26))
+
+(posn-x p)
+(posn-x (make-posn 31 26))
