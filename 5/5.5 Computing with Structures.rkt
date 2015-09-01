7f1713c7744f1e825ef004865f5ad683dbab46a0
krzychukula <krzychukula@gmail.com>
2015-09-01 08:40:28 +0200

    5.5 Laws

---
+++ 5/5.5 Computing with Structures.rkt
@@ -0,0 +1,34 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname |5.5 Computing with Structures|) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp")) #f)))
+
+; STRUCT entry
+(define-struct entry [name phone email])
+
+(make-entry "Sarah Lee" "666-7771" "lee@classy-university.edu")
+
+(define pl
+  (make-entry "Sarah Lee" "666-7771" "lee@classy-university.edu"))
+ 
+(entry-name pl)
+
+
+;(entry-name (make-posn 42 5))
+;entry-name: expects an entry, given (posn 42 5)
+
+
+(define-struct ball [location velocity])
+(define-struct vel [deltax deltay])
+
+(define ball1 (make-ball (make-posn 30 40) (make-vel -10 5)))
+
+(ball-velocity ball1)
+(vel-deltax (ball-velocity ball1))
+
+;Exercise 70. Spell out the laws that the following structure type definitions introduce:
+(define-struct centry [name home office cell])
+(define-struct phone [area number])
+
+;(centry-name (make-centry v1 v2 v3 v4)) == v1
+;(phone-area (make-phone v1 v2)) == v1
+
