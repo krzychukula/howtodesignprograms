ecd745728fa59a4d5ca90a5b6a70e47f18ef2970
krzychukula <krzychukula@gmail.com>
2015-09-01 08:45:43 +0200

    5.5 Predicates

--- 5/5.5 Computing with Structures.rkt
+++ 5/5.5 Computing with Structures.rkt
@@ -7,10 +7,7 @@
 
 (make-entry "Sarah Lee" "666-7771" "lee@classy-university.edu")
 
-(define pl
-  (make-entry "Sarah Lee" "666-7771" "lee@classy-university.edu"))
- 
-(entry-name pl)
+
 
 
 ;(entry-name (make-posn 42 5))
@@ -31,4 +28,57 @@
 
 ;(centry-name (make-centry v1 v2 v3 v4)) == v1
 ;(phone-area (make-phone v1 v2)) == v1
+
+
+
+
+; Predicates
+
+(define a-posn (make-posn 7 0))
+ 
+(define pl
+  (make-entry "Sarah Lee" "666-7771" "lee@classy-university.edu"))
+
+(posn? a-posn)
+(posn? 42)
+(posn? #true)
+(posn? (make-posn 3 4))
+
+
+(entry? pl)
+(entry? 42)
+(entry? #true)
+
+
+;Exercise 71. Place the following definitions in DrRacket’s definition area:
+(define HEIGHT 200)
+(define MIDDLE (quotient HEIGHT 2))
+(define WIDTH  400)
+(define CENTER (quotient WIDTH 2))
+ 
+(define-struct game [left-player right-player ball])
+ 
+(define game0
+  (make-game MIDDLE MIDDLE (make-posn CENTER CENTER)))
+
+;Click RUN and evaluate the following expressions:
+(game-ball game0)
+(posn? (game-ball game0))
+(game-left-player game0)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
